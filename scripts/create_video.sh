e20e15b7e20a6c870e4aa1c165e416199499252b
brandon-jernigan <brandonjernigan@arizona.edu>
2023-09-20 21:57:36 -0700

    updated ffmpeg line to allow skiping frames

--- scripts/create_video.sh
+++ scripts/create_video.sh
@@ -13,4 +13,4 @@ mkdir -p videos
 current_datetime=$(date +"%Y%m%d%H%M%S")
 
 # Create the video using FFmpeg
-ffmpeg -framerate 30 -pattern_type glob -i 'frames/frame_*.png' -c:v libx264 -pix_fmt yuv420p "videos/output_${current_datetime}.mp4"
+ffmpeg -framerate 60 -pattern_type glob -i 'frames/frame_*.png' -vf "select=not(mod(n\,1))" -c:v libx264 -pix_fmt yuv420p "videos/output_${current_datetime}.mp4"
