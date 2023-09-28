997d492b7e2e04a39a18e4a03fdc04533622da6a
Brandon Jernigan <brandonjernigan@arizona.edu>
2023-09-27 18:03:51 -0700

    new min reproductive cost

--- src/cell.rs
+++ src/cell.rs
@@ -134,7 +134,7 @@ impl Cell {
         let mut nucleus_color = hsva_to_rgba(h, s, v, a);
         let (heading, speed) = velocity_to_polar(x_vel, y_vel);
         let mut new_reproductive_cost = 0.0;
-        while new_reproductive_cost < 25.0 {
+        while new_reproductive_cost < 36.0 {
             new_reproductive_cost = reproductive_cost + rng.gen_range(-2.0..2.0);
         }
         let light_consumtion_efficiency = mass / 2000.0;
